ce45881e27daa25467ddfbd1ebf774b46e0c16c6
denis <mrisholigare@gmail.com>
2021-02-16 20:06:47 +0300

    more workings on the body

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(number1, number2) {
+    var results = number1 + number2;
+    return results;
+}
+var combineAges = add("denis", "ligare");
+console.log(combineAges);
---
+++ app.ts
@@ -0,0 +1,6 @@
+function add(number1:string,number2:string){
+const results =number1+number2
+return results
+}
+const combineAges =add("denis","ligare");
+console.log(combineAges);
